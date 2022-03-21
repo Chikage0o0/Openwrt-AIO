c1fc3779b2ee53ebe116868e80348ecd6c44d169
Chikage0o0 <89348590+Chikage0o0@users.noreply.github.com>
2022-03-21 15:42:57 +0800

    Change CONFIG_TARGET_SQUASHFS_BLOCK_SIZE

--- diy/sh/target/ramips-mt7621.sh
+++ diy/sh/target/ramips-mt7621.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sed -i '/CONFIG_TARGET_SQUASHFS_BLOCK_SIZE/ c\CONFIG_TARGET_SQUASHFS_BLOCK_SIZE=128' .config
 
 # 编译固件
 make image PROFILE="youhua_wr1200js" PACKAGES="luci luci-lib-ipkg -dnsmasq dnsmasq-full luci-i18n-opkg-zh-cn luci-i18n-firewall-zh-cn luci-i18n-base-zh-cn luci-compat luci-lib-ipkg -kmod-usb3 -kmod-usb-ledtrig-usbport" FILES="files"
