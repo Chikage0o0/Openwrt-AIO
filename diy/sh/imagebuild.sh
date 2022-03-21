1d8a4ba9d87811904d3c2d91e2696b05ce99792e
Chikage0o0 <89348590+Chikage0o0@users.noreply.github.com>
2022-03-21 16:40:28 +0800

    fix publickey

--- diy/sh/imagebuild.sh
+++ diy/sh/imagebuild.sh
@@ -4,7 +4,7 @@ sudo chown -R 1000:1000 /home/build
 # 添加签名验证
 sudo chmod +x tool/usign
 cp tool/public.key keys/$(./tool/usign -F -p tool/public.key)
-cp tool/public.key files/etc/opkg/keys/$(./tool/usign -F -p tool/public.key)
+mkdir -p files/etc/opkg/keys && cp tool/public.key files/etc/opkg/keys/$(./tool/usign -F -p tool/public.key)
 
 bash target.sh
 
