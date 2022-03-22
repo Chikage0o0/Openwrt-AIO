2debc52cec6ccfb65d92788d0ffcfe459200c52f
Chikage0o0 <89348590+Chikage0o0@users.noreply.github.com>
2022-03-22 08:29:07 +0800

    test rockchip r2s

--- diy/sh/target/rockchip-armv8.sh
+++ diy/sh/target/rockchip-armv8.sh
@@ -4,5 +4,4 @@ sed -i '/CONFIG_TARGET_KERNEL_PARTSIZE/ c\CONFIG_TARGET_KERNEL_PARTSIZE=64' .con
 sed -i '/CONFIG_TARGET_ROOTFS_PARTSIZE/ c\CONFIG_TARGET_ROOTFS_PARTSIZE=448' .config
 
 # 编译固件
-make image PROFILE="generic" PACKAGES="luci luci-lib-ipkg -dnsmasq dnsmasq-full luci-i18n-opkg-zh-cn luci-i18n-firewall-zh-cn luci-i18n-base-zh-cn luci-compat kmod-igc kmod-vmxnet3 kmod-r8125" FILES="files"
-
+make image PROFILE="generic" PACKAGES="luci luci-lib-ipkg -dnsmasq dnsmasq-full luci-i18n-opkg-zh-cn luci-i18n-firewall-zh-cn luci-i18n-base-zh-cn luci-compat" FILES="files"
