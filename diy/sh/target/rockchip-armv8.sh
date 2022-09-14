6c69f06af88d330e76302f225479a1d8e90ea74b
Chikage0o0 <89348590+Chikage0o0@users.noreply.github.com>
2022-09-14 10:23:36 +0800

    Update rockchip-armv8.sh

--- diy/sh/target/rockchip-armv8.sh
+++ diy/sh/target/rockchip-armv8.sh
@@ -4,4 +4,4 @@ sed -i '/CONFIG_TARGET_KERNEL_PARTSIZE/ c\CONFIG_TARGET_KERNEL_PARTSIZE=64' .con
 sed -i '/CONFIG_TARGET_ROOTFS_PARTSIZE/ c\CONFIG_TARGET_ROOTFS_PARTSIZE=448' .config
 
 # 编译固件
-make image PROFILE="friendlyarm_nanopi-r2s" PACKAGES="luci luci-lib-ipkg -dnsmasq dnsmasq-full luci-i18n-opkg-zh-cn luci-i18n-firewall-zh-cn luci-i18n-base-zh-cn luci-compat" FILES="files"
+make image PROFILE="friendlyarm_nanopi-r2s" PACKAGES="luci luci-lib-ipkg -dnsmasq dnsmasq-full luci-i18n-opkg-zh-cn luci-i18n-firewall-zh-cn luci-i18n-base-zh-cn luci-compat kmod-nft-fullcone" FILES="files"
